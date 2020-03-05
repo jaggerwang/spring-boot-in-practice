05b7443b09f585e0201e603b5d295c764921e7e4
Jagger Wang <jaggerwang@gmail.com>
2020-03-05 17:36:05 +0800

    using @RestControllerAdvice

--- src/main/java/net/jaggerwang/sbip/adapter/controller/RestExceptionHandler.java
+++ src/main/java/net/jaggerwang/sbip/adapter/controller/RestExceptionHandler.java
@@ -1,45 +1,45 @@
 package net.jaggerwang.sbip.adapter.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import net.jaggerwang.sbip.adapter.controller.dto.RootDto;
 import net.jaggerwang.sbip.usecase.exception.UnauthenticatedException;
 import net.jaggerwang.sbip.usecase.exception.UnauthorizedException;
 import net.jaggerwang.sbip.usecase.exception.NotFoundException;
 import net.jaggerwang.sbip.usecase.exception.UsecaseException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler(Throwable.class)
-    public ResponseEntity<RootDto> handle(Throwable throwable) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RootDto handle(Throwable throwable) {
         throwable.printStackTrace();
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new RootDto("fail", throwable.toString()));
+        return new RootDto("fail", throwable.toString());
     }
 
     @ExceptionHandler(UsecaseException.class)
-    public ResponseEntity<RootDto> handle(UsecaseException exception) {
-        return ResponseEntity.ok().body(new RootDto("fail", exception.getMessage()));
+    @ResponseStatus(HttpStatus.OK)
+    public RootDto handle(UsecaseException exception) {
+        return new RootDto("fail", exception.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<RootDto> handle(NotFoundException exception) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new RootDto("not_found", exception.getMessage()));
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public RootDto handle(NotFoundException exception) {
+        return new RootDto("not_found", exception.getMessage());
     }
 
     @ExceptionHandler(UnauthenticatedException.class)
-    public ResponseEntity<RootDto> handle(UnauthenticatedException exception) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new RootDto("unauthenticated", exception.getMessage()));
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public RootDto handle(UnauthenticatedException exception) {
+        return new RootDto("unauthenticated", exception.getMessage());
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<RootDto> handle(UnauthorizedException exception) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(new RootDto("unauthorized", exception.getMessage()));
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public RootDto handle(UnauthorizedException exception) {
+        return new RootDto("unauthorized", exception.getMessage());
     }
 }
