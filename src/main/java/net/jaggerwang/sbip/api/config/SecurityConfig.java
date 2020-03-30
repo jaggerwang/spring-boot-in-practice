dded814dbba1a3a2c05fb92ad372f73cade97570
Jagger Wang <jaggerwang@gmail.com>
2020-03-30 13:31:42 +0800

    set default authentication entry point

--- src/main/java/net/jaggerwang/sbip/api/config/SecurityConfig.java
+++ src/main/java/net/jaggerwang/sbip/api/config/SecurityConfig.java
@@ -1,27 +1,18 @@
 package net.jaggerwang.sbip.api.config;
 
-import java.io.IOException;
-import javax.servlet.http.HttpServletResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import net.jaggerwang.sbip.adapter.controller.dto.RootDto;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private ObjectMapper objectMapper;
-
-    public SecurityConfig(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -33,34 +24,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    private void responseJson(HttpServletResponse response, HttpStatus status, RootDto data) throws IOException {
-        response.setStatus(status.value());
-        response.setContentType(MediaType.APPLICATION_JSON_UTF8.toString());
-        response.getWriter().write(objectMapper.writeValueAsString(data));
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                // TODO: It will cause '/login' page not found.
-//                .exceptionHandling(exceptionHandling -> exceptionHandling
-//                        .authenticationEntryPoint((request, response, authException) ->
-//                                responseJson(response, HttpStatus.UNAUTHORIZED,
-//                                        new RootDto("unauthenticated", "未认证")))
-//                        .accessDeniedHandler((request, response, accessDeniedException) ->
-//                                responseJson(response, HttpStatus.FORBIDDEN,
-//                                        new RootDto("unauthorized", "未授权")))
-//                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .defaultAuthenticationEntryPointFor(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                                new AntPathRequestMatcher("/**"))
+                )
                 .authorizeRequests(authorizeRequests -> authorizeRequests
                         .antMatchers("/favicon.ico", "/csrf", "/vendor/**", "/webjars/**",
                                 "/actuator/**", "/v2/api-docs", "/swagger-ui.html",
                                 "/swagger-resources/**", "/", "/graphql", "/login", "/logout",
                                 "/auth/login", "/auth/logout", "/auth/logged", "/user/register",
-                                "/files/**")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated()
+                                "/files/**").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .formLogin(formLogin -> {})
                 .logout(logout -> {});
